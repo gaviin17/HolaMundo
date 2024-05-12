4bfdb430a96b556ab0820f731039898536a10bbe
gaviin17 <pepinthegamer@gmail.com>
2024-05-12 10:55:08 +0200

    Saludo y nombre
    Saludo y nombre de java

---
+++ hellogit.java
@@ -0,0 +1,8 @@
+public class Hellogit {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Como estas");
+        System.out.println("Me llamo Iván");
+    }
+}
